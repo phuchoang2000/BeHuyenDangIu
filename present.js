54d48dc2d87bfd2edd78b4eacd90999fd2200b72
phuchoang2000 <phuchoangnguyen1812@gmail.com>
2022-08-20 14:03:34 +0700

    your message

---
+++ present.js
@@ -0,0 +1,49 @@
+
+var to = 'Bé Hằng!';
+var gift_url = '';
+var gift_image_url = 'hihi.jpg';
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.toggle('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
+document.getElementById('card').addEventListener('click',function(){
+  document.getElementById('card').classList.toggle('card-show');
+  present.classList.toggle("open");
+})
